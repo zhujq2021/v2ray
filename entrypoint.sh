6529ad4bdfe0821497f9db6dfcc08d89c6d74534
zhujq <38308874+zhujq@users.noreply.github.com>
2020-10-21 22:41:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@
 #fi
 
 cd /v2ray
-wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+wget -O v2ray.zip http://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip v2ray.zip 
 if [ ! -f "v2ray" ]; then
   mv /v2ray/v2ray-v$VER-linux-64/v2ray .
