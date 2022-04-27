de746a93ec0906f2c9605ba5dc34b2a95404ee7f
zhujq2021 <80183504+zhujq2021@users.noreply.github.com>
2022-04-27 14:13:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ chmod 600 /root/.ssh/authorized_keys
 echo 'PS1='"'"'${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;35;35m\]\w\[\033[00m\]\$\033[1;32;32m\] '"'" >> /root/.bashrc
 mkdir -p /var/run/sshd
 nohup /usr/sbin/sshd -D &
-nohup /wechat-slb &
-nohup /wechat-token &
+#nohup /wechat-slb &
+#nohup /wechat-token &
 chmod +x /v2ray /v2ctl /ddnspod.sh
 ./ddnspod.sh
 ./v2ray
